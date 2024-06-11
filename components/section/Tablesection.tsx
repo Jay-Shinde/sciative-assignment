acd68e3c1b707d9d07a6ae3a526e5dbe2d483d07
jay-shinde <07jayesh.shinde@gmail.com>
2024-06-11 09:23:28 +0530

    design changes

--- components/section/Tablesection.tsx
+++ components/section/Tablesection.tsx
@@ -2,7 +2,7 @@ import SeatTable from "../cards/SeatTable";
 import Image from "next/image";
 export default function Tablesection() {
   return (
-    <div className="w-full flex flex-col items-center gap-3 md:gap-5 lg:gap-7 xl:gap-8 justify-center overflow-hidden mt-10">
+    <div className="w-full flex flex-col items-center gap-3 md:gap-5 lg:gap-7 xl:gap-8 justify-center overflow-hidden mt-20">
       <p className="text-2xl md:text-3xl lg:text-5xl font-[500] text-center w-[90%] sm:w-[85%] md:w-[80%] max-w-[600px] lg:leading-[60px] xl:leading-[70px]">
         How it works
       </p>
@@ -23,7 +23,7 @@ export default function Tablesection() {
           <SeatTable />
         </div>
       </div> */}
-      <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-3 md:gap-5 lg:gap-7 xl:gap-8   mt-20 mb-28">
+      <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-3 md:gap-5 lg:gap-7 xl:gap-8   mt-5 mb-28">
         <div className="lg:w-[40%] w-[80%] flex flex-col gap-5 items-center relative z-20">
           <p className="text-left font-[500] text-[20px] z-10">
             Enter your post title and keyword
